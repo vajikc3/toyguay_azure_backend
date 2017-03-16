db51bc0a765cc78ac4df6783eb27c11afe8d295e
AOA <akixe.otegi@sgsmap.com>
2017-03-16 05:52:41 +0100

    prueba 170316055238

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
- var http = require('http')
- var port = process.env.PORT || 1337;
- http.createServer(function(req, res) {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end('Hello World\n');
- }).listen(port);
+var http = require('http')
+var azure = require('azure-storage');
+var blobService = azure.createBlobService();
+blobService.createContainerIfNotExists('taskcontainer', {
+  publicAccessLevel: 'blob'
+}, function(error, result, response) {
+  if (!error) {
+        var port = process.env.PORT || 1337;
+        http.createServer(function(req, res) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end("Resultado:" + result);
+        }).listen(port);
+    // if result = true, container was created.
+    // if result = false, container already existed.
+  }
+});
+
+ 
