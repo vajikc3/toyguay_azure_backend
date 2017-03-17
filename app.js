0df0f57244a5648fc5e92c3222f1f0d63341899d
AOA <akixe.otegi@sgsmap.com>
2017-03-17 06:44:17 +0100

    Organizes app.js

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
+
+/* -- MULTER -- */
 var multer  = require('multer');
+var memStorage = multer.memoryStorage();
+var multerUpload = multer({ storage: memStorage });
 
-
-
+/* -- AZURE -- */
+var azureSorage = require('azure-storage');
+var blobService = azureSorage.createBlobService();
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', multerUpload.single('file'), getImage);
+
+/* -- INIT SERVER -- */
 var port = process.env.PORT || 1337;
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
+
+
+/* ===== IMPL ===== */
+function getImage(req, res){
+    res.send('Hello World!');
+}
