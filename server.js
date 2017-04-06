4afcf3a5d5eb36c9881387b09db8bc2b1b4c8569
AOA <akixe.otegi@sgsmap.com>
2017-04-06 06:35:42 +0200

    Irrelevant changes (cleaning)

--- server.js
+++ server.js
@@ -14,35 +14,3 @@ blobService.createContainerIfNotExists('taskcontainer', {
     // if result = false, container already existed.
   }
 });
-
-// var multer  = require('multer');
-// var storage = multer.memoryStorage();
-// var upload = multer({ storage: storage });
-// var azureSorage = require('azure-storage');
-// var blobSvc = azureSorage.createBlobService();
-// var streamifier = require('streamifier');
-
-// router.post('/imageupload', upload.single('file'),function(req, res, next){
-//        console.log(req.file,'----files--',req.body);
-
-//         var stream = streamifier.createReadStream(req.file.buffer);
-//         console.log(stream)
-
-//         blobSvc.createBlockBlobFromStream(
-//             'images',
-//             req.file.originalname,
-//             stream,
-//             req.file.size,
-//             function(error, result, response){
-//                 console.log(error,result,response);
-//                 if(error){
-//                     console.log("Couldn't upload stream");
-//                     console.error(error);
-//                 } else {
-//                     console.log('Stream uploaded successfully');
-//                     req.model.data = response;
-//                     next();
-//                 }
-//             });
-
-//     });
